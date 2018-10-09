61bd033d7cb8bef6e68afb81e29b43c13d238814
Ijay <ij.oguegbu@gmail.com>
2018-10-09 13:26:45 -0300

    Use PORT from environment

--- UrlShortenerServer.py
+++ UrlShortenerServer.py
@@ -41,6 +41,7 @@
 #
 # After writing each step, restart the server and run test.py to test it.
 
+import os
 import http.server
 import requests
 from urllib.parse import unquote, parse_qs
@@ -151,6 +152,7 @@ class Shortener(http.server.BaseHTTPRequestHandler):
             self.wfile.write("URI entered is not valid".encode())
 
 if __name__ == '__main__':
-    server_address = ('', 5000)
+    port = int(os.environ.get('PORT', 8000))
+    server_address = ('', port)
     httpd = http.server.HTTPServer(server_address, Shortener)
     httpd.serve_forever()
